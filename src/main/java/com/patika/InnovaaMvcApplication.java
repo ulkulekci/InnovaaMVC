8969e4ac877f404810481a4ea07a52b28ada60b7
ulkukuleci <ulkusulu07@hotmail.com>
2022-02-01 22:19:30 +0300

    Spring MVC First Commit

---
+++ src/main/java/com/patika/InnovaaMvcApplication.java
@@ -0,0 +1,13 @@
+package com.patika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnovaaMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnovaaMvcApplication.class, args);
+    }
+
+}
